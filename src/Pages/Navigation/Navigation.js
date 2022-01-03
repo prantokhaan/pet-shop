9a0101f67a719d32f92fe32579f08d76ee77b04c
sabbir0029 <mdsabbirhossain0029@gmail.com>
2022-01-03 11:35:09 +0600

    code pull origin main

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -14,7 +14,7 @@ const Navigation = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/home" className='text-dark'>Home</Nav.Link>
-              <Nav.Link as={Link} to="/Services" className='text-dark'>Services</Nav.Link>
+              <Nav.Link as={Link} to="/About" className='text-dark'>About Us</Nav.Link>
               <Nav.Link as={Link} to="/MyOders" className='text-dark'>My Oders</Nav.Link>
             </Nav>
             <Nav>
